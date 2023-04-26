a39d494c55c047fb25dcff1fa553d4e581cc8231
RaviSatyam <rsatyam1296@gmail.com>
2023-04-27 00:41:27 +0530

    check emitter existance implemented

--- controllers/emitter-controller.js
+++ controllers/emitter-controller.js
@@ -34,5 +34,18 @@ const getAllEmitters=async(req,res)=>{
        return res.status(500).json({message:error.message})
     }
 };
+const checkEmitterExistance=async(req,res)=>{
+    const {accountId}=req.params;
+    const emitter=await Emitter.findOne({accountId:accountId});
+    console.log(emitter);
+    if(emitter){
+        try{
+            return res.status(200).json({message:"This account ID already exist !"});
+        } catch(error){
+            return res.status(500).json({message:error});
+        }
+    }
+    return res.status(404).json({message:"This user does not exist !"})
+}
 
-module.exports={registerEmitter,getAllEmitters};
+module.exports={registerEmitter,getAllEmitters,checkEmitterExistance};
--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,12 @@
 const express=require("express");
 const router=express.Router();
 
-const {registerEmitter,getAllEmitters}=require("../controllers/emitter-controller.js");
+const {registerEmitter,getAllEmitters,checkEmitterExistance}=require("../controllers/emitter-controller.js");
 const {registerGovt,getGovtDetails}=require("../controllers/govt-controller.js");
 // routes for emitter
 router.route("/addEmitter").post(registerEmitter);
 router.route("/getAllEmitter").get(getAllEmitters);
+router.route("/checkEmitterExistance/:accountId").get(checkEmitterExistance);
 
 // routes for government
 router.route("/addGovt").post(registerGovt);
